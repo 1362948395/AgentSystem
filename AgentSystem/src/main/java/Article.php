e5045fd95a811a8346d0d6e9fa02b98919382afb
1362948395 <m17137282609@163.com>
2018-02-01 19:47:53 +0800

    sd

---
+++ AgentSystem/src/main/java/Article.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2018/2/1
+ * Time: 19:46
+ */
+
+
+class Article{
+    public function __construct()
+    {
+        echo 'v1.0';
+    }
+}
+?>
