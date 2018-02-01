424b4e53ea02bd33ca0fb8eec1c5c8a7e1820139
1362948395 <m17137282609@163.com>
2018-02-01 18:31:58 +0800

    sdasds

---
+++ AgentSystem/src/main/java/Artical.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2018/2/1
+ * Time: 18:28
+ */
+
+class Artical
+{
+    public function __construct()
+    {
+        echo "hellow word";
+    }
+}
